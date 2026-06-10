d2322ef355cf8c5fd3b2a1f0755f2b54444d1ab7
DevinRussell01 <devinrussell01@outlook.com>
2026-06-10 18:56:21 +0000

    updated logic for clevend county

--- cleveland_gis_test.py
+++ cleveland_gis_test.py
@@ -3,18 +3,18 @@ import requests
 url = "https://gis.clevelandcounty.com/arcgis/rest/services/Tax/Tax/FeatureServer/1/query"
 
 params = {
-    "where": "GIS_Calculated_Acres >= 25",
+    "where": "GIS_Deeded_Acres >= 25",
     "outFields": "GIS_PID,GIS_PIN,GIS_Owner1,GIS_Owner2,GIS_Calculated_Acres,GIS_Deeded_Acres,GIS_X_Coord,GIS_Y_Coord",
     "returnGeometry": "false",
     "f": "json",
     "resultRecordCount": 25,
-    "orderByFields": "GIS_Calculated_Acres DESC"
+    "orderByFields": "GIS_Deeded_Acres DESC"
 }
 
 response = requests.get(url, params=params)
 data = response.json()
 
-print("\nLargest Cleveland Parcels:\n")
+print("\nLargest Cleveland Parcels (Deeded Acres):\n")
 
 for feature in data.get("features", []):
     attr = feature.get("attributes", {})
@@ -22,7 +22,10 @@ for feature in data.get("features", []):
     print("Owner:", attr.get("GIS_Owner1"))
     print("PID:", attr.get("GIS_PID"))
     print("PIN:", attr.get("GIS_PIN"))
-    print("Acres:", attr.get("GIS_Calculated_Acres"))
+
+    print("Deeded Acres:", attr.get("GIS_Deeded_Acres"))
+    print("Calculated Acres:", attr.get("GIS_Calculated_Acres"))
+
     print("X:", attr.get("GIS_X_Coord"))
     print("Y:", attr.get("GIS_Y_Coord"))
     print("------")
